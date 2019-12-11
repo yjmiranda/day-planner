1584a1c1c5f45f96a1d7e899ea63b6a043ce295d
Yali Miranda <yalimiranda@gmail.com>
2019-12-10 17:22:11 -0800

    added background color depending on time, added event listener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 $("#current-day").text(moment().format("dddd, MMM Do"));
 var container = $("#schedule-container");
+if (localStorage.getItem("day-planner") !== null){
+    schedule = JSON.parse(localStorage.getItem("day-planner"));
+}
 
 for(var i = 0; i < schedule.length; i++){
 
@@ -18,11 +21,23 @@ for(var i = 0; i < schedule.length; i++){
     var textArea = $("<textarea>");
     textArea.attr("rows","3");
     textArea.attr("data-time", (i+9));
-    textArea.addClass("form-control bg-light border-white border-left-0");
+    textArea.attr("id", ("textarea-"+i));
+    textArea.addClass("form-control border-white border-left-0");
+    if(parseInt(textArea.attr("data-time")) < parseInt(moment().format("H"))){
+        textArea.addClass("bg-light");
+    }
+    if(parseInt(textArea.attr("data-time")) > parseInt(moment().format("H"))){
+        textArea.addClass("bg-success text-white");
+    }
+    if(parseInt(textArea.attr("data-time")) === parseInt(moment().format("H"))){
+        textArea.addClass("bg-danger text-white");
+    }
+    textArea.text(schedule[i].task);
     textDiv.append(textArea);
 
     var button = $("<div>");
     button.addClass("btn btn-info col-3 col-sm-2 col-md-1");
+    button.attr("id",i)
     row.append(button);
     var buttonImg = $("<img>");
     buttonImg.addClass("button-img");
@@ -30,4 +45,13 @@ for(var i = 0; i < schedule.length; i++){
     buttonImg.attr("alt", "Save");
     button.append(buttonImg);
 }
-  
+
+function saveTask(event){
+    var buttonID = parseInt($(this).attr("id"));
+    var textID = $("#textarea-"+buttonID);
+    
+    schedule[buttonID].task = textID.val();
+    localStorage.setItem("day-planner", JSON.stringify(schedule));
+}
+
+$(document).on("click", ".btn", saveTask);  
